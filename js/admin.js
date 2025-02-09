d38897a621549d53380b9daf034f231b78526b02
MikeNgo93 <michaelfromvietnam@gmail.com>
2025-02-09 18:59:11 +0700

    update

--- js/admin.js
+++ js/admin.js
@@ -3,7 +3,6 @@ const BASE_URL = "https://67925d36cf994cc68049c413.mockapi.io/products";
 /*
 Các cải tiến: 
   - Dùng chung một cấu trúc async function để dễ đọc hơn
-  - Không dùng class trong trường hợp này vì cảm thấy dùng từng function là đủ ở front end
   - Dùng try - catch thay vì then - catch như trong lớp học 
   - Có dùng finally để tắt loading
 */
